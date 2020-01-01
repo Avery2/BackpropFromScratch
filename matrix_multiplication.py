3c59dee6b68a67d15e58af65eaf8af714b042daa
Avery2 <JustinAveryChan@gmail.com>
2020-01-01 01:02:49 -0600

    value storage implemented with arrays

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -3,32 +3,43 @@
 
 
 class Matrix:
-    def __init__(self, height=0, width=0):
-        self.__height = height
-        self.__width = width
+    def __init__(self, height=0, width=0, values=[]):
+        self._values = values
+        self._height = height
+        self._width = width
+        for x in range(0, height):
+            values.append([])
+            for y in range(0, width):
+                print(f"({x}, {y})")
+                values[x].append(0)
+
+    @property
+    def values(self):
+        return self._values
 
     @property
     def height(self):
-        return self.__height
+        return self._height
 
     @property
     def width(self):
-        return self.__width
+        return self._width
 
     @width.setter
     def set_width(self, value):
         if value.isdigit():
-            self.__width = value
+            self._width = value
         else:
             raise ValueError("please enter a digit")  # unsure of terminology
 
     @height.setter
     def set_height(self, value):
         if value.isdigit():
-            self.__height = value
+            self._height = value
         else:
             raise ValueError("please enter a digit")  # unsure of terminology
 
 
 my_matrix = Matrix(5, 5)
 print(f"height {my_matrix.height} width {my_matrix.width}")
+print(my_matrix.values)
